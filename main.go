4b57303d7c32d8544487db900e35e6bb42aa610c
Akash <akashsingh2210670@gmail.com>
2024-11-23 01:02:57 +0530

    feat: Add logo and greeting message to server startup

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"github.com/example/controller"
+	"fmt"
+
+	"github.com/SkySingh04/fractal/controller"
 	"gofr.dev/pkg/gofr"
+)
+
+const (
+	logo = `
+ ______ _____            _____ _______       _      
+ |  ____|  __ \     /\   / ____|__   __|/\   | |     
+ | |__  | |__) |   /  \ | |       | |  /  \  | |     
+ |  __| |  _  /   / /\ \| |       | | / /\ \ | |     
+ | |    | | \ \  / ____ \ |____   | |/ ____ \| |____ 
+ |_|    |_|  \_\/_/    \_\_____|  |_/_/    \_\______|
+`
 )
 
 func main() {
 	app := gofr.New()
+	fmt.Println(logo)
+
+	// Print greeting message when the server starts
+	app.Logger().Infof("Server is starting... Welcome to the Fractal API!")
 
 	// register route greet
 	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-
 		return "Hello World!", nil
 	})
 
