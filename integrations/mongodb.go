00eb3b924816eb8a9660441660ed837b4adca62a
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-11-23 20:21:52 +0530

    add concurrency to mongodb

--- integrations/mongodb.go
+++ integrations/mongodb.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"sync"
 
 	"github.com/SkySingh04/fractal/interfaces"
 	"github.com/SkySingh04/fractal/logger"
@@ -12,6 +13,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const bufferSize = 10 // Buffer size for channels
 
 // MongoDBSource struct represents the configuration for consuming messages from MongoDB.
 type MongoDBSource struct {
@@ -53,20 +56,48 @@ func (m MongoDBSource) FetchData(req interfaces.Request) (interface{}, error) {
 	}
 	defer cursor.Close(context.TODO())
 
+	// Use a buffered channel to handle data
+	dataChannel := make(chan bson.M, bufferSize)
+	var wg sync.WaitGroup
 	var allResults []bson.M
-	for cursor.Next(context.TODO()) {
-		var result bson.M
-		if err := cursor.Decode(&result); err != nil {
-			return nil, err
+	var fetchError error
+	mutex := &sync.Mutex{}
+
+	// Goroutine to collect data from the channel
+	go func() {
+		for doc := range dataChannel {
+			mutex.Lock()
+			allResults = append(allResults, doc)
+			mutex.Unlock()
 		}
-		allResults = append(allResults, result)
+	}()
+
+	// Process each document
+	for cursor.Next(context.TODO()) {
+		var doc bson.M
+		if err := cursor.Decode(&doc); err != nil {
+			fetchError = err
+			break
+		}
+
+		wg.Add(1)
+		go func(d bson.M) {
+			defer wg.Done()
+			dataChannel <- d
+		}(doc)
 	}
 
+	wg.Wait()
+	close(dataChannel)
+
+	if fetchError != nil {
+		return nil, fetchError
+	}
 	if err := cursor.Err(); err != nil {
 		return nil, err
 	}
 
-	logger.Infof("Data fetched from MongoDB: %v", allResults)
+	logger.Infof("Data fetched from MongoDB: %d documents", len(allResults))
 	return allResults, nil
 }
 
@@ -97,12 +128,41 @@ func (m MongoDBDestination) SendData(data interface{}, req interfaces.Request) e
 		return errors.New("invalid data format: expected []bson.M")
 	}
 
-	for _, row := range dataSlice {
-		if _, err := collection.InsertOne(context.TODO(), row); err != nil {
-			logger.Errorf("Error inserting into collection %s: %v", req.TargetMongoDBCollection, err)
-			return err
+	// Buffered channel for sending documents
+	dataChannel := make(chan bson.M, bufferSize)
+	errorChannel := make(chan error, bufferSize)
+	var wg sync.WaitGroup
+
+	// Goroutines for worker pool
+	for i := 0; i < bufferSize; i++ { // Worker pool
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for doc := range dataChannel {
+				if _, err := collection.InsertOne(context.TODO(), doc); err != nil {
+					logger.Errorf("Error inserting into collection %s: %v", req.TargetMongoDBCollection, err)
+					errorChannel <- err
+				} else {
+					logger.Infof("Data sent to MongoDB target collection %s: %v", req.TargetMongoDBCollection, doc)
+				}
+			}
+		}()
+	}
+
+	// Feed data into the channel
+	go func() {
+		for _, doc := range dataSlice {
+			dataChannel <- doc
 		}
-		logger.Infof("Data sent to MongoDB target collection %s: %v", req.TargetMongoDBCollection, row)
+		close(dataChannel)
+	}()
+
+	wg.Wait()
+	close(errorChannel)
+
+	// Check for errors in the error channel
+	if len(errorChannel) > 0 {
+		return errors.New("one or more errors occurred while inserting data")
 	}
 
 	return nil
